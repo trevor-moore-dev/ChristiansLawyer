92d75efe91e4c49c58c5c6936826db1ea816c7cd
Trevor Moore <trevomoo@gmail.com>
2024-06-11 21:46:21 -0700

    added theme preference to cookies

--- christians-lawyer/src/app/theme.js
+++ christians-lawyer/src/app/theme.js
@@ -2,14 +2,27 @@
 import { useState, useMemo, createContext } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Cookies from 'js-cookie';
+
+function getThemeMode() {
+    const themeMode = Cookies.get('themeMode')?.toLowerCase();
+    if (themeMode === 'light' || themeMode === 'dark') {
+        return themeMode;
+    }
+    return 'light';
+}
 
 export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 export default function Theme({ children }) {
-    const [mode, setMode] = useState('light');
+    const [mode, setMode] = useState(getThemeMode());
     const colorMode = useMemo(() => ({
         toggleColorMode: () => {
-            setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            setMode(prevMode => {
+                const newMode = (prevMode === 'light') ? 'dark' : 'light';
+                Cookies.set('themeMode', newMode, { expires: 365 });
+                return newMode;
+            });
         }
     }), []);
 
